bd6776c5186191e38a11b934c4b0804583609716
exepta <genshin.exepta@outlook.com>
2023-09-03 14:17:11 +0200

    create project

---
+++ src/main/java/net/exsorce/webpanel/WebPanelApplication.java
@@ -0,0 +1,13 @@
+package net.exsorce.webpanel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebPanelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebPanelApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/exsorce/webpanel/WebPanelApplicationTests.java
@@ -0,0 +1,13 @@
+package net.exsorce.webpanel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebPanelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
