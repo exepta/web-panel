1e660cc308b3a82d059361d7087be245789527d5
exepta <genshin.exepta@outlook.com>
2023-09-05 13:46:55 +0200

    update login

--- web-frontend/src/main/components/Authentication.tsx
+++ web-frontend/src/main/components/Authentication.tsx
@@ -1,5 +1,5 @@
 import '../../resources/pages/auth/auth.css';
-import {useState} from "react";
+import {useEffect, useRef, useState} from "react";
 
 import GoogleIcon from '@mui/icons-material/Google';
 import GitHubIcon from '@mui/icons-material/GitHub';
@@ -7,10 +7,60 @@ import TwitterIcon from '@mui/icons-material/Twitter';
 
 import MailOutlineIcon from '@mui/icons-material/MailOutline';
 import LockIcon from '@mui/icons-material/Lock';
+import PersonIcon from '@mui/icons-material/Person';
+import VerifiedUserIcon from '@mui/icons-material/VerifiedUser';
+
+const USER_NAME_PATTERN = /^[a-zA-Z][a-zA-Z0-9-_]{3,25}$/;
+const PASSWORD_PATTERN = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)([!@#$%^&*<>/?]).{8,}$/;
 
 const Authentication = () => {
 
+    const emailRef :any = useRef();
     const [swap, setSwap] = useState(false);
+
+    const [email, setEmail] = useState('');
+    const [validEmail, setValidEmail] = useState(false);
+
+    const [username, setUsername] = useState('');
+    const [validUsername, setValidUsername] = useState(false);
+
+    const [password, setPassword] = useState('');
+    const [validPassword, setValidPassword] = useState(false);
+
+    useEffect(() => {
+        emailRef.current.focus();
+    }, []);
+
+    useEffect(() => {
+        const result = email.includes('@');
+        setValidEmail(result);
+    }, [email]);
+
+    useEffect(() => {
+        const result = USER_NAME_PATTERN.test(username);
+        setValidUsername(result);
+    }, [username]);
+
+    useEffect(() => {
+        const result = PASSWORD_PATTERN.test(password);
+        setValidPassword(result);
+    }, [password]);
+
+    const handleLogin :any = async (event :Event) => {
+        event.preventDefault();
+
+        const checkMail = email.includes('@');
+        const checkPassword = PASSWORD_PATTERN.test(password);
+        if(!checkMail || !checkPassword) {
+            return;
+        }
+
+        //Todo: Fetch here
+    }
+
+    const handleRegister :any = async (event :Event) => {
+        event.preventDefault();
+    }
 
     return (
         <>
@@ -20,26 +70,34 @@ const Authentication = () => {
                         <div className="sec-header">
                             <h1>Welcome</h1>
                         </div>
-                    </section>
 
-                    :
-
-                    <section id="section-log">
-                        <div className="sec-header">
-                            <h1>Welcome</h1>
-                        </div>
-
-                        <form>
+                        <form onSubmit={handleRegister}>
                             <div className="input-con">
                                 <div className="icon-box">
                                     <MailOutlineIcon className="icon" />
                                 </div>
                                 <input
                                     type="email"
-                                    id="log-email"
+                                    id="reg-email"
+                                    ref={emailRef}
                                     placeholder="E-Mail"
                                     required
                                     autoComplete="off"
+                                    onChange={(event) => setEmail(event.target.value)}
+                                />
+                            </div>
+
+                            <div className="input-con">
+                                <div className="icon-box">
+                                    <PersonIcon className="icon" />
+                                </div>
+                                <input
+                                    type="text"
+                                    id="reg-user"
+                                    placeholder="Username"
+                                    required
+                                    autoComplete="off"
+                                    onChange={(event) => setUsername(event.target.value)}
                                 />
                             </div>
 
@@ -53,11 +111,69 @@ const Authentication = () => {
                                     placeholder="Password"
                                     required
                                     autoComplete="off"
+                                    onChange={(event) => setPassword(event.target.value)}
                                 />
                             </div>
 
                             <div className="text-con">
-                                <p>Forgot password?</p>
+                                <p>Problems by creating new account?</p>
+                            </div>
+
+                            <button type="submit" className="send">Sign Up</button>
+
+                            <h3>Already a member or social login?</h3>
+                            <div className="switch" onClick={event => {setSwap(false)}}>Login</div>
+                        </form>
+                        <div className="sec-footer">
+                            <div className="container">
+                                <p>Impress</p>
+                            </div>
+                        </div>
+                    </section>
+
+                    :
+
+                    <section id="section-log">
+                        <div className="sec-header">
+                            <h1>Welcome</h1>
+                        </div>
+
+                        <form onSubmit={handleLogin}>
+                            <div className="input-con">
+                                <div className="icon-box">
+                                    <MailOutlineIcon className="icon" />
+                                </div>
+                                <input
+                                    type="email"
+                                    id="log-email"
+                                    ref={emailRef}
+                                    placeholder="E-Mail"
+                                    required
+                                    autoComplete="off"
+                                    onChange={(event) => setEmail(event.target.value)}
+                                />
+                            </div>
+
+                            <div className="input-con">
+                                <div className="icon-box">
+                                    <LockIcon className="icon" />
+                                </div>
+                                <input
+                                    type="password"
+                                    id="log-email"
+                                    placeholder="Password"
+                                    required
+                                    autoComplete="off"
+                                    onChange={(event) => setPassword(event.target.value)}
+                                />
+                            </div>
+
+                            <div className="text-con">
+                                <div className="remember">
+                                    <VerifiedUserIcon className="check"/>
+                                    <p>Remember me</p>
+                                </div>
+                                <p className="forgot">Forgot password?</p>
                             </div>
 
                             <button type="submit" className="send">Login</button>
@@ -77,7 +193,15 @@ const Authentication = () => {
                                     <GitHubIcon />
                                 </div>
                             </div>
+
+                            <h3>Or</h3>
+                            <div className="switch" onClick={event => {setSwap(true)}}>Sign Up</div>
                         </form>
+                        <div className="sec-footer">
+                            <div className="container">
+                                <p>Impress</p>
+                            </div>
+                        </div>
                     </section>
             }
         </>
